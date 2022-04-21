b3d0f0df7447cc609b7841dfc918065ddb22729a
ZALAK THAKOR <72291815+zalakthakor@users.noreply.github.com>
2022-04-21 11:50:03 +0530

    Add files via upload

---
+++ MEAN-SHOP-CART/Client/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProductsComponent } from './component/products/products.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'products', component: ProductsComponent},
+  {path:'cart', component: CartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
